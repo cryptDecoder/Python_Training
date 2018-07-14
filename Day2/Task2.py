9a436228891906a5ee3b1297439c049be0543cd1
cryptDecoder <pruthvirajs2007@gmail.com>
2018-07-14 19:41:29 +0530

    Initial commit

---
+++ Day2/Task2.py
@@ -0,0 +1,15 @@
+'''Looping statements
+foor loop in list'''
+
+
+st_name = ['ram','shyam','goa',12,23.56,'True']
+print(len(st_name))
+
+print(range(20))
+
+for i in range(10):
+    print(i)
+
+
+for student in st_name:
+    print(student)
