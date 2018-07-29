a30d61b49525abb67532bd6b8dade4e4afe3d89b
cryptDecoder <pruthvirajs2007@gmail.com>
2018-07-29 14:14:45 +0530

    Initial Commit

--- Day7/Lambda_Function.py
+++ Day7/Lambda_Function.py
@@ -1,10 +1,16 @@
 '''
 lambda =
 syntax :
-lambda =
+lambda arguments : expression
 '''
 
 
 f = lambda x,y:x+y
 
 print(f(1,5))
+
+no = lambda x,y:x**y
+print(no(3,2))
+
+nos = lambda x:x**x
+print(nos(3))
---
+++ Day7/Map.py
@@ -0,0 +1,11 @@
+def max(x):
+    return x*4
+
+print(list(map(max,[2,4,5,6])))
+
+
+
+def sumation(x,y):
+    return x+y
+
+print(list(map(sumation,[12])))
